aca2252c7792464cbed8550c4c30abe6792fbcc7
ArnautVasseur <vasseur.arnaut@gmail.com>
2024-02-14 17:49:14 +0100

    update origin of corsOptions

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 4000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://projet-s6.onrender.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
